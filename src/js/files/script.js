81c6ef2b408f61528a079c35f02097ccccf209df
Wlad199 <habilis199@mail.ru>
2023-06-12 19:52:14 +0300

    addPagination

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -33,15 +33,33 @@ $(document).ready(function () {
 	});
 })
 
+//////////////////////////////////////////////////////////////////
+// aside menu
 
 let buttonAsideOpen = document.querySelector('.open-menu');
 let asideMenu = document.querySelector('.aside');
-//let buttonAsideClose = document.querySelector('.aside-menu-close')
-
 function activeAside() {
 	asideMenu.classList.toggle('_active')
 	buttonAsideOpen.classList.toggle('_active')
 }
-
 buttonAsideOpen.addEventListener('click', activeAside)
-//buttonAsideClose.addEventListener('click', activeAside)
+
+////////////////////////////////////////////////////////////////////
+// Pagination
+
+let toSecondPage = document.querySelector('.pagination-second');
+let toSecondSecondPage = document.querySelector('.pagination-secondd');
+let toFirstPage = document.querySelector('.pagination-first');
+let parentBlock = document.querySelector('.block__container');
+
+function addClassSecond() {
+	parentBlock.classList.add('_second-page')
+}
+
+function removeClassSecond() {
+	parentBlock.classList.remove('_second-page')
+}
+
+toSecondPage.addEventListener('click', addClassSecond)
+toSecondSecondPage.addEventListener('click', addClassSecond)
+toFirstPage.addEventListener('click', removeClassSecond)
