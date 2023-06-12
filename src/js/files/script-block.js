f212cb5cb34fb0734b8d45aa00e323562558d5da
Wlad199 <habilis199@mail.ru>
2023-06-12 20:24:22 +0300

    +++fin+++

--- src/js/files/script-block.js
+++ src/js/files/script-block.js
@@ -1,41 +1,6 @@
-// Подключение функционала "Чертогов Фрилансера"
-import { isMobile } from "./functions.js";
-// Подключение списка активных модулей
-import { flsModules } from "./modules.js";
-
-
-// Функционал слайдера-слик (MW 3.2)
-$(document).ready(function () {
-	$('.main-screen__box').slick({
-		dots: true, // точки
-		arrows: false,
-		autoplay: true,
-	});
-})
-
-$(document).ready(function () {
-	$('.comment-slider').slick({
-		dots: false, // точки
-		arrows: true,
-		slidesToShow: 1,
-	});
-})
-
-
-$(document).ready(function () {
-	$('.gallery-contact__box').slick({
-		dots: true, // точки
-		arrows: false,
-		slidesToShow: 6,
-		autoplay: true, // автовоспроизведение (false)
-		autoplaySpeed: 3000, // скорость (3000)
-		variableWidth: true,
-	});
-})
 
 //////////////////////////////////////////////////////////////////
-//aside menu
-/*
+// aside menu
 
 let buttonAsideOpen = document.querySelector('.open-menu');
 let asideMenu = document.querySelector('.aside');
@@ -44,11 +9,9 @@ function activeAside() {
 	buttonAsideOpen.classList.toggle('_active')
 }
 buttonAsideOpen.addEventListener('click', activeAside);
-*/
 
 //////////////////////////////////////////////////////////////////////
 // Pagination
-/*
 
 let toSecondPage = document.querySelector('.pagination-second');
 let toSecondSecondPage = document.querySelector('.pagination-secondd');
@@ -66,4 +29,3 @@ function removeClassSecond() {
 toSecondPage.addEventListener('click', addClassSecond)
 toSecondSecondPage.addEventListener('click', addClassSecond)
 toFirstPage.addEventListener('click', removeClassSecond)
-*/
